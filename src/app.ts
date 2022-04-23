1ba3f27e60ba601c42bae623b8587da712bfe1b3
Uzezi Jephter <kaskidsky@gmail.com>
2022-04-22 21:41:30 -0700

    added the ability to get input value from form

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,41 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     innerElement: HTMLFormElement;
 
+    titleInputElement: HTMLInputElement;                                    
+    descriptionInputElement: HTMLInputElement;                                    
+    peopleInputElement: HTMLInputElement;                                    
+
     constructor() {
         this.rootElement = document.getElementById('app')! as HTMLDivElement;
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.innerElement = document.importNode(this.templateElement.content, true).firstElementChild as HTMLFormElement;
         this.innerElement.id = "user-input";
+        this.titleInputElement = this.innerElement.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.innerElement.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputElement = this.innerElement.querySelector('#people')! as HTMLInputElement;
+        this.innerElement.addEventListener('submit', this.submitHandler.bind(this));
         this.attach();
 
     }
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+        }   
+    }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        if(enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input, please try again');
+            return;
+        }
+        return [enteredTitle, enteredDescription, +enteredPeople];
+    }
     private attach() {
         this.rootElement.appendChild(this.innerElement);
     }
