3295e9414b769f6a4c9942addcc0f3d861fcae52
Uzezi Jephter <kaskidsky@gmail.com>
2022-04-24 01:09:48 -0700

    added class heirarchy for reusability

--- src/app.ts
+++ src/app.ts
@@ -10,26 +10,106 @@ function Autobind(_:Object, _2:string, descriptor:PropertyDescriptor) {
     return adjDescriptor;
 }
 
-class ProjectInput {
-    rootElement: HTMLDivElement;
+type Listener<T> = (projects: T[]) => {};
+
+class State<T>{
+    
+    private listeners: Listener<T>[];
+    private state: T[];
+
+    public constructor(){
+        this.listeners = [];
+        this.state = [];
+    }
+    
+
+    public addData (data: T){
+        this.state.push(data);
+        this.updateObservers();
+    }
+
+    public addListeners(listener: Listener<T>){
+        this.listeners.push(listener);
+    }
+    private updateObservers(){
+        for(let listener of this.listeners){
+            listener(this.state);
+        }
+    }
+
+}
+
+enum ProjectStatus{
+    Active = "active", Finished="finished"
+}
+
+class Project{
+    public id: string;
+    constructor(public title:string, public description: string, public people: number, public status: ProjectStatus){
+        this.id = Math.random().toString(16);
+    }
+}
+
+class ProjectState extends State<Project>{
+    private static instance: ProjectState;
+    private constructor(){
+        super();
+    }
+    public static getInstance(){
+        if(this.instance) return this.instance;
+        this.instance = new ProjectState();
+        return this.instance;
+
+    }
+}
+
+abstract class Component<T extends HTMLElement, U extends HTMLElement>{
+    rootElement: T;
     templateElement: HTMLTemplateElement;
-    innerElement: HTMLFormElement;
+    innerElement: U;
+    insertionPosition: "afterbegin"|"beforeend";
+    constructor(templateId: string, hostId: string, insertionPosition: "afterbegin"|"beforeend", newelementId?:string){
+        this.rootElement = document.getElementById(hostId)! as T;
+        this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+        this.innerElement = document.importNode(this.templateElement.content, true).firstElementChild as U;
+        if(newelementId)
+        this.innerElement.id = newelementId;
+        this.insertionPosition= insertionPosition;
+
+        this.attach();
+    }
+
+    private attach() {
+        this.rootElement.insertAdjacentElement(this.insertionPosition,this.innerElement);
+    }
+
+    public abstract configure(): void;
+    public abstract renderComponent(): void;
+}
+
+
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
 
     titleInputElement: HTMLInputElement;                                    
     descriptionInputElement: HTMLInputElement;                                    
     peopleInputElement: HTMLInputElement;                                    
-
+    projectState: State<Project>;
     constructor() {
-        this.rootElement = document.getElementById('app')! as HTMLDivElement;
-        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-        this.innerElement = document.importNode(this.templateElement.content, true).firstElementChild as HTMLFormElement;
-        this.innerElement.id = "user-input";
+       super("project-input", "app", "afterbegin","user-input");
+        this.projectState = ProjectState.getInstance();
         this.titleInputElement = this.innerElement.querySelector('#title')! as HTMLInputElement;
         this.descriptionInputElement = this.innerElement.querySelector('#description')! as HTMLInputElement;
         this.peopleInputElement = this.innerElement.querySelector('#people')! as HTMLInputElement;
-        this.innerElement.addEventListener('submit', this.submitHandler);
-        this.attach();
+        this.configure();
+        
 
+    }
+
+    public configure(): void {
+        this.innerElement.addEventListener('submit', this.submitHandler);
+    }
+    public renderComponent(): void {
+        
     }
     @Autobind
     private submitHandler(event: Event) {
@@ -37,7 +117,8 @@ class ProjectInput {
         const userInput = this.gatherUserInput();
         if (Array.isArray(userInput)) {
             const [title, description, people] = userInput;
-            console.log(title, description, people);
+            this.projectState.addData(new Project(title, description,people, ProjectStatus.Active));
+            this.clearFields();
         }   
     }
 
@@ -45,14 +126,20 @@ class ProjectInput {
         const enteredTitle = this.titleInputElement.value;
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
+        console.log(enteredDescription, enteredPeople, enteredTitle)
         if(enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
             alert('Invalid input, please try again');
             return;
         }
         return [enteredTitle, enteredDescription, +enteredPeople];
     }
-    private attach() {
-        this.rootElement.appendChild(this.innerElement);
+    
+
+    private clearFields() {
+         this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.peopleInputElement.value = "";
+        
     }
 
 }
